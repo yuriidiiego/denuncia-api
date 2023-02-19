0f0589b5d2709ab41615ba4a79486e0aa0cf0de6
Yuri Nascimento <yuriidiiego@gmail.com>
2023-02-19 19:40:59 -0300

    Refactor DenunciaController to use valid request and response objects

--- src/main/java/br/com/run2biz/denuncia/adapters/DenunciaController.java
+++ src/main/java/br/com/run2biz/denuncia/adapters/DenunciaController.java
@@ -1,64 +1,64 @@
 package br.com.run2biz.denuncia.adapters;
 
+import br.com.run2biz.denuncia.domain.denuncia.IDenunciaService;
+import br.com.run2biz.denuncia.domain.denuncia.payload.request.CreateDenunciaRequest;
+import br.com.run2biz.denuncia.domain.denuncia.payload.response.ResponseDenuncia;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.run2biz.denuncia.domain.denuncia.IDenunciaService;
-import br.com.run2biz.denuncia.domain.denuncia.payload.request.CreateDenuncia;
-import br.com.run2biz.denuncia.domain.denuncia.payload.response.ResponseDenuncia;
-
 @RestController
 @RequestMapping("/v1/denuncias")
-@Tag(name = "Denúncias", description = "Controla as denúncias")
+@Tag(name = "Denúncias", description = "Endpoint para gerenciar as denúncias")
 public class DenunciaController {
 
-  private final IDenunciaService service;
+  private final IDenunciaService denunciaService;
 
-  public DenunciaController(IDenunciaService service) {
-    this.service = service;
+  public DenunciaController(IDenunciaService denunciaService) {
+    this.denunciaService = denunciaService;
   }
 
-  @Operation(
-    description = "Criar uma nova denúncia",
-    operationId = "createDenuncia"
-  )
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  @Operation(summary = "Criar nova denúncia")
   @ApiResponses(
     value = {
       @ApiResponse(
         responseCode = "201",
         description = "Denúncia criada com sucesso"
       ),
-      @ApiResponse(responseCode = "400", description = "Solicitação inválida"),
+      @ApiResponse(responseCode = "400", description = "Requisição inválida"),
       @ApiResponse(responseCode = "409", description = "Conflito de recurso"),
       @ApiResponse(
         responseCode = "500",
-        description = "Erro interno do servidor"
+        description = "Erro interno no servidor"
       ),
     }
   )
-  @PostMapping
   public ResponseEntity<ResponseDenuncia> createDenuncia(
-    @Parameter(
-      required = true,
-      description = "Corpo da denúncia"
-    ) @RequestBody CreateDenuncia denuncia
+    @Valid @RequestBody CreateDenunciaRequest createDenunciaRequest
   ) {
-    ResponseDenuncia response = service.criarDenuncia(denuncia);
+    ResponseDenuncia responseDenuncia = denunciaService.criarDenuncia(
+      createDenunciaRequest
+    );
+
     URI location = ServletUriComponentsBuilder
       .fromCurrentRequest()
       .path("/{id}")
-      .buildAndExpand(response.getId())
+      .buildAndExpand(responseDenuncia.getId())
       .toUri();
-    return ResponseEntity.created(location).body(response);
+
+    return ResponseEntity.created(location).body(responseDenuncia);
   }
 }
