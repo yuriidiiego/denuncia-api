1cfc203b9b70b795e0a58b6a03fbb0c1fdbe0008
Yuri Nascimento <yuriidiiego@gmail.com>
2023-02-19 19:45:49 -0300

    Rename 'CreateDenuncia' to 'CreateDenunciaRequest'

--- src/main/java/br/com/run2biz/denuncia/domain/denuncia/DenunciaMapper.java
+++ src/main/java/br/com/run2biz/denuncia/domain/denuncia/DenunciaMapper.java
@@ -3,14 +3,14 @@ package br.com.run2biz.denuncia.domain.denuncia;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import br.com.run2biz.denuncia.domain.denuncia.payload.request.CreateDenuncia;
+import br.com.run2biz.denuncia.domain.denuncia.payload.request.CreateDenunciaRequest;
 import br.com.run2biz.denuncia.domain.denuncia.payload.response.ResponseDenuncia;
 
 @Mapper(componentModel = "spring")
 public interface DenunciaMapper {
   @Mapping(target = "id", ignore = true)
   @Mapping(target = "endereco", ignore = true)
-  Denuncia toDenuncia(CreateDenuncia request);
+  Denuncia toDenuncia(CreateDenunciaRequest request);
 
   ResponseDenuncia toResponseDenuncia(Denuncia denuncia);
 }
