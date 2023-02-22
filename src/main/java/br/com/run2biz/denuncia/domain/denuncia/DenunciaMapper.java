bb851d1cda1243501799f296e0a7dae2cd1aa4a3
Yuri Nascimento <yuriidiiego@gmail.com>
2023-02-21 21:06:17 -0300

    Update DenunciaMapper with new response class and method name
    The DenunciaMapper interface was updated to reflect changes in the response class and method name. The response class was updated from ResponseDenuncia to DenunciaResponse, and the method name was updated from toResponseDenuncia to just toResponse. The functionality remains the same, mapping a Denuncia object to a CreateDenunciaRequest object and vice versa, but the updated names reflect clearer and more consistent naming conventions.

--- src/main/java/br/com/run2biz/denuncia/domain/denuncia/DenunciaMapper.java
+++ src/main/java/br/com/run2biz/denuncia/domain/denuncia/DenunciaMapper.java
@@ -4,13 +4,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import br.com.run2biz.denuncia.domain.denuncia.payload.request.CreateDenunciaRequest;
-import br.com.run2biz.denuncia.domain.denuncia.payload.response.ResponseDenuncia;
+import br.com.run2biz.denuncia.domain.denuncia.payload.response.DenunciaResponse;
 
 @Mapper(componentModel = "spring")
 public interface DenunciaMapper {
   @Mapping(target = "id", ignore = true)
   @Mapping(target = "endereco", ignore = true)
-  Denuncia toDenuncia(CreateDenunciaRequest request);
+  Denuncia toDenuncia(CreateDenunciaRequest createDenunciaRequest);
 
-  ResponseDenuncia toResponseDenuncia(Denuncia denuncia);
+  DenunciaResponse toResponseDenuncia(Denuncia denuncia);
 }
